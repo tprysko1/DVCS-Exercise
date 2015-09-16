980e1e4fb20eca775fd71608da78f5eb99fe5859
Tyler Prysko <tprysko1@studentmail.nait.ca>
2015-09-16 10:29:41 -0600

    Start Project - Blank website

---
+++ DVCS_Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_Exercise.Startup))]
+namespace DVCS_Exercise
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
